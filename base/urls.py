25038d8ebf27aa8fa01184a4814026dbe1ea70b2
Aleksandr Novomlinov <aleksandr.novomlinov@monobank.ua>
2024-04-02 21:19:50 +0300

    add api

--- base/urls.py
+++ base/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.models import CategoryResource
+from api.models import CourseResource
+from tastypie.api import Api
+
+api = Api(api_name='v1')
+course_resource = CourseResource()
+category_resource = CategoryResource()
+api.register(course_resource)
+api.register(category_resource)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+    path('api/', include(api.urls))
+    # path('api/', include(course_resource.urls)),
+    # path('api/', include(category_resource.urls))
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from . import models
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Course
 
 # Create your views here.
 
 
 def index(request):
-    courses = models.Course.objects.all()
+    courses = Course.objects.all()
     # return HttpResponse([''.join(course.title + '<br>') for course in courses])
     return render(request, 'courses.html',
                   {
@@ -16,9 +16,12 @@ def index(request):
 
 
 def single_course(request, course_id):
-    course = models.Course.objects.get(pk=course_id)
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+    course = get_object_or_404(Course, pk=course_id)
     return render(request, 'single_course.html', {'course': course})
-
-
 
 
